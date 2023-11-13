b104288e9b488d7fd34d2d57fb5d9896996bec6f
johnakis45 <johnakis45@gmail.com>
2023-11-13 07:09:21 +0200

    Added prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,38 @@
-import { App } from './app';
+// import { App } from '../src/app';
 
-const application = new App();
-application.start();
+// const application = new App();
+// application.start();
 
-export { application };
+// export { application };
+import express, { NextFunction, Request, Response } from "express";
+import 'dotenv/config'
+import bodyParser from "body-parser";
+//import main_router from "./api/index";
+
+const app = express()
+const port = process.env.PORT;
+
+app.use(bodyParser.json())
+
+//app.use("/api", main_router)
+
+app.use(printer)
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!')
+    console.log(req.body.__id)
+    next()
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+function printer(req: Request, res: Response, next: NextFunction) {
+
+    const id = "123"
+    req.body.__id = id;
+    next();
+}
+
