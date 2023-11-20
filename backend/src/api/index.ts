175414a848bd1f5c12c1201422c70f41b107ec4c
johnakis45 <johnakis45@gmail.com>
2023-11-20 16:24:33 +0200

    init

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,30 +1,21 @@
-// import * as express from 'express';
-// import { apiV1Router } from './v1';
+import * as express from 'express';
+import { apiV1Router } from './v1';
 
-// export class Api {
+export class Api {
 
-//     /**
-//      * Apply all app routes including models and auth
-//      *
-//      * @param {express.Application} app
-//      * @returns {Promise<express.Router>}
-//      */
-//     public static async applyRoutes(app: express.Application): Promise<express.Router> {
+    /**
+     * Apply all app routes including models and auth
+     *
+     * @param {express.Application} app
+     * @returns {Promise<express.Router>}
+     */
+    public static async applyRoutes(app: express.Application): Promise<express.Router> {
 
-//         const apiRouter = express.Router();
+        const apiRouter = express.Router();
 
-//         apiRouter.use('/api/', apiV1Router);
+        apiRouter.use('/api/', apiV1Router);
 
-//         return apiRouter;
-//     }
+        return apiRouter;
+    }
 
-// }
-
-import { Router } from "express";
-import itemRouter from "./item/item";
-
-const router = Router();
-
-router.use("/item", itemRouter);
-
-export default router;
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,6 @@
-// import { App } from '../src/app';
+import { App } from './app';
 
-// const application = new App();
-// application.start();
+const application = new App();
+application.start();
 
-// export { application };
-import express, { NextFunction, Request, Response } from "express";
-import 'dotenv/config'
-import bodyParser from "body-parser";
-import main_router from "./api/index";
-
-const app = express()
-const port = process.env.PORT;
-
-app.use(bodyParser.json())
-
-app.use("/api", main_router)
-
-app.use(printer)
-
-app.get('/', (req, res, next) => {
-    res.send('Hello Worldddd!')
-    console.log(req.body.__id)
-    next()
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-function printer(req: Request, res: Response, next: NextFunction) {
-    const id = "123"
-    req.body.__id = id;
-    next();
-}
-
-
-
+export { application };
